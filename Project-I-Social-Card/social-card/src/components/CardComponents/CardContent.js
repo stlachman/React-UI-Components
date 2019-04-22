b079f3c63b2ed4e4c9922026bbc81f9d9a620b7f
Seth Lachman <lachmanseth17@gmail.com>
2019-04-22 16:55:35 -0400

    Added styles to card container components.

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -7,7 +7,7 @@ const CardContent = () => {
     <div className="card-content">
       <h4>Get started with React</h4>
       <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
-      <p className="light-grey">reactjs.org</p>
+      <span className="light-grey">reactjs.org</span>
     </div>
   )
 }
