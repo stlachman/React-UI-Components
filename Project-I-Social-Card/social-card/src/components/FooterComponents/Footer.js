132d73108dfaabed74b54a7c65cd17d3e6cd4bc1
Seth Lachman <lachmanseth17@gmail.com>
2019-04-22 19:11:43 -0400

    Corrected className.

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,23 +1,24 @@
 import React from "react";
 import "./Footer.css";
 
+
 const Footer = () => {
   return (
     <div className="footer">
       <div className="container">
         <button className="footer-button">
-            <i class="far fa-comment fa-flip-horizontal"></i>
+            <i className="far fa-comment fa-flip-horizontal"></i>
         </button>
         <button className="footer-button">
-            <i class="fas fa-sync"></i>
+            <i className="fas fa-sync"></i>
             <span>6</span>
         </button>
         <button className="footer-button">
-            <i class="far fa-heart"></i>
+            <i className="far fa-heart"></i>
             <span>4</span>
         </button>
         <button className="footer-button">
-            <i class="far fa-envelope"></i>
+            <i className="far fa-envelope"></i>
         </button>
       </div>
     </div>
---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/FooterButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function FooterButton(props) {
+  return (
+    <button className="footer-button">
+        <i class="far fa-comment fa-flip-horizontal"></i>
+    </button>
+  );
+}
+
+export default FooterButton;
