6f380ae142ef859005d95667399c6bb77c1ed05c
Seth Lachman <lachmanseth17@gmail.com>
2019-04-22 15:44:11 -0400

    Created first header copmonents.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <div className="header-title">
+      <span class="full-name">Lambda School</span>
+      <span className="username">@LambdaSchool</span>
+      <span className="date"> 26 jan</span>
+    </div>
+  )
+};
+
+export default HeaderTitle;
