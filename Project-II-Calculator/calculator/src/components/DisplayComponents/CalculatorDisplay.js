027c63b4bb6b02673c4f2648dd8cfd10c5bc877d
Seth Lachman <lachmanseth17@gmail.com>
2019-04-23 15:27:50 -0400

    Created initial display component.

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Display.css';
+
+
+const CalculatorDisplay = (props) => {
+  console.log(props);
+  return (
+    <div className="calculator-display">
+      {props.result}
+    </div>
+  )
+};
+
+export default CalculatorDisplay;
